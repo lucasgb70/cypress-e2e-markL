33f3dccd1f22d477a6fd8358f48171acd76c7d90
Lucas G. Budniewski <lucasgb73@gmail.com>
2024-11-03 14:45:36 -0300

    Removendo fakerjs e passando a trabalhar com dados gerenciados pela API, delete de tasks

--- cypress-express-mark/cypress/e2e/tasks..cy.js
+++ cypress-express-mark/cypress/e2e/tasks..cy.js
@@ -1,13 +1,21 @@
 /// <reference types="cypress"/>
 
-import {faker} from '@faker-js/faker'
 describe('tarefas', ()=>{
     
     it('deve cadastrar uma nova tarefa', ()=>{
+
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: {name: 'Ler um livro de Node.js'}
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
         cy.visit('http://localhost:8080')
 
         cy.get('input[placeholder="Add a new Task"]')
-            .type(faker.music.songName())
+            .type('Ler um livro de Node.js')
 
         //Não precisa do plugin xPath se usar as funções do Cypress
         cy.contains('button', 'Create').click()

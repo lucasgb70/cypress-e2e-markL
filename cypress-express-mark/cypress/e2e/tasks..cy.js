f3b8a821af21a3c4546640343b28dc81f3e51f0f
Lucas G. Budniewski <lucasgb73@gmail.com>
2024-11-03 16:06:43 -0300

    Inserindo teste de duplicação de tasks e independencia dos testes

--- cypress-express-mark/cypress/e2e/tasks..cy.js
+++ cypress-express-mark/cypress/e2e/tasks..cy.js
@@ -25,4 +25,38 @@ describe('tarefas', ()=>{
 
     })
 
+    it('Não deve permitir tarefa duplicada', ()=>{
+    
+        cy.request({
+            url: 'http://localhost:3333/helper/tasks',
+            method: 'DELETE',
+            body: {name: 'Estudar Javascript'}
+        }).then(response => {
+            expect(response.status).to.eq(204)
+        })
+
+        //Dado que eu tenho uma tarefa duplicada
+        cy.request({
+            url: 'http://localhost:3333/tasks',
+            method: 'POST',
+            body: {name: 'Estudar Javascript', is_done: false}
+        }).then(response => {
+            expect(response.status).to.eq(201)
+        })
+
+        //Quando faço o cadastro desta tarefa
+        cy.visit('http://localhost:8080')
+
+        cy.get('input[placeholder="Add a new Task"]')
+        .type('Estudar Javascript')
+
+        cy.contains('button', 'Create').click()
+
+        //Então vejo a mensagem de duplicidade
+                
+        cy.get('.swal2-html-container')
+            .should('be.visible')
+            .should('have.text', 'Task already exists!')
+    })
+
 })
